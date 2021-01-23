608d1d3f74a37961a0eed216bdff0996c6dbc639
lenyn29 <lenyn.alfonzo@gmail.com>
2021-01-23 14:36:29 -0400

    exp

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ const fetchData = async (id) => {
             img: data.sprites.other.dream_world.front_default,
             nombre: data.name,
             vida: data.stats[0].base_stat,
-            tipo: data.types[1].type.name,
+            exp: data.base_experience,
             ataque: data.stats[1].base_stat,
             defensa: data.stats[2].base_stat,
             especial: data.stats[3].base_stat
@@ -41,7 +41,7 @@ const pintarCard = (pokemon) => {
 
     clone.querySelector('.card-body-img').setAttribute('src', pokemon.img)
     clone.querySelector('.card-body-title').innerHTML = `${pokemon.nombre}  <span>${pokemon.vida} hp </span>`
-    clone.querySelector('.card-body-text').innerHTML = `${pokemon.tipo}`
+    clone.querySelector('.card-body-text').innerHTML = `${pokemon.exp} Exp`
     clone.querySelectorAll('.card-footer-social h3')[0].textContent = `${pokemon.ataque}K`
     clone.querySelectorAll('.card-footer-social h3')[1].textContent = `${pokemon.defensa}K`
     clone.querySelectorAll('.card-footer-social h3')[2].textContent = `${pokemon.especial}K`
