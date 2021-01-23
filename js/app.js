1d153c69899e41f5250d8e82cb41fc4d93d6edb6
lenyn29 <lenyn.alfonzo@gmail.com>
2021-01-23 00:49:17 -0400

    poke api

---
+++ js/app.js
@@ -0,0 +1,46 @@
+console.log('dame like');
+
+const getRandomInt = (min,max) => {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const random = getRandomInt(1, 151)
+    fetchData(random)
+})
+
+const fetchData = async (id) => {
+    try {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        const data = await res.json()
+        console.log(data)
+        const pokemon = {
+            img: data.sprites.other.dream_world.front_default,
+            nombre: data.name,
+            vida: data.stats[0].base_stat,
+            exp: data.base_experience,
+
+        }
+
+        pintarCard(pokemon)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const pintarCard = (pokemon) => {
+    console.log(pokemon)
+    const flex = document.querySelector('.flex')
+    const template = document.querySelector('#template-card').content
+    const clone = template.cloneNode(true)
+    const fragment = document.createDocumentFragment();
+
+    clone.querySelector('.card-body-img').setAttribute('src', pokemon.img)
+    clone.querySelector('.card-body-title').innerHTML = `${pokemon.nombre}  <span>${pokemon.vida} hp </span>`
+    clone.querySelector('.card-body-text').textContent = `${pokemon.exp} exp`
+
+    fragment.appendChild(clone)
+    flex.appendChild(fragment)
+
+}
